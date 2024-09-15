442012bf5727aa11fa7428aa299fb2fe8f0997b0
Yogi Ardiansyah Pratama <141385631+yogiardiansyahP@users.noreply.github.com>
2024-09-15 13:07:39 +0700

    Add files via upload

---
+++ storage/framework/views/e87fa0ceb9116d39616e1760ce53f3a2.php
@@ -0,0 +1,5 @@
+<div <?php echo e($attributes->class(['fi-dropdown-list p-1'])); ?>>
+    <?php echo e($slot); ?>
+
+</div>
+<?php /**PATH C:\Users\yogi\laravel_filament\vendor\filament\support\src\/../resources/views/components/dropdown/list/index.blade.php ENDPATH**/ ?>
